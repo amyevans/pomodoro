65c92c2ccc601d16e3895917dc03558661a777ce
Amy Evans <amyevans87@gmail.com>
2015-10-14 15:47:56 -0500

    Stop at 0, add ability to increase session and pause timer

--- script.js
+++ script.js
@@ -6,19 +6,45 @@ $(document).ready(function() {
 		$clock = $('.clock'),
 		$countdown = $('#countdown'),
 		intervalID,
-		time = 121,
+		time = 1500, // 25 minute default, 25 * 60 = 1500
 		formattedTime = '';
+
+
+	// add time to session on click
+	$addSession.on('click', function () {
+		time += 60;
+		displayTime();
+	});
+
+	// subtract time from session on click
+	$subtractSession.on('click', function () {
+		time -= 60;
+		displayTime();
+	});
 
 	// if you click on the clock face, it sets the interval
 	$clock.on('click', function () {
-		// every 1000 millisceonds (1 sec), updateCountdown() is called
-		intervalID = setInterval(updateCountdown, 1000);
+		if (!intervalID) {
+			// every 1000 milliseconds (1 sec), updateCountdown() is called
+			intervalID = setInterval(updateCountdown, 1000);
+		// pause the timer if it's clicked while running
+		} else {
+			clearInterval(intervalID);
+			intervalID = 0;
+		}
 	});
 
 	function updateCountdown() {
+		if (time <= 0) {
+			clearInterval(intervalID);
+		}
+		time -= 1;
+		displayTime();
+	}
+
+	function displayTime() {
 		formattedTime = formatTime(time);
 		$countdown.html(formattedTime);
-		time -= 1;
 	}
 
 	// take seconds and format for display (h:mm:ss or m:ss)
@@ -26,11 +52,11 @@ $(document).ready(function() {
 		var hours = 0,
 			minutes = 0;
 
-		while ( seconds > 3600 ) {
+		while ( seconds >= 3600 ) {
 			hours += 1;
 			seconds -= 3600;
 		}
-		while ( seconds > 60 ) {
+		while ( seconds >= 60 ) {
 			minutes += 1;
 			seconds -= 60;
 		}
